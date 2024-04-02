dca9558e9909f33ff6874c36f36b9a0bbcdaac19
Alejandro <alejhandro.de@gmail.com>
2024-04-01 21:17:17 -0600

    adding success message when the fidelity card gets the free service

---
+++ js/config.js
@@ -0,0 +1,6 @@
+var CONFIG = {
+    APP: {
+        FIDELITY_API_URL: "https://script.google.com/a/~/macros/s/AKfycbxx6Drb-MiKQ2QyuH6JD8Dtb1pN74tnk9Jw7fLl6fsu-nFA_BVB0v3jvRI1aVIJZQ2sbQ/exec",
+        GIFTCARD_API_URL: "https://script.google.com/a/~/macros/s/AKfycbx-ZPqMh7C3rPUCxAqBZElyG4dUJ0w4ZSwp9VjSy-nZ1b4oM-Olbcj4R-RpLKC3sMPyug/exec"
+    }
+}
