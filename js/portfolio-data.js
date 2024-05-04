3d80d2a38890678e97906483d7bbf0e4dc3506e9
Alejandro <alejhandro.de@gmail.com>
2024-05-03 20:46:26 -0600

    adding promo of the month into fidelity card page. adding -10% off in 4th visit for fidelity card. adding more lashes images to portfolio

--- js/portfolio-data.js
+++ js/portfolio-data.js
@@ -1,16 +1,6 @@
 var portfolio_data = {
     nails: [
         {
-            name: 'nails_2',
-            url: 'images/portfolio/unas_2.jpeg',
-            show: true
-        },
-        {
-            name: 'nails_1',
-            url: 'images/portfolio/unas_1.jpeg',
-            show: false
-        },
-        {
             name: 'nails_3',
             url: 'images/portfolio/unas_3.jpeg',
             show: true
@@ -19,6 +9,16 @@ var portfolio_data = {
             name: 'nails_4',
             url: 'images/portfolio/unas_4.jpeg',
             show: true
+        },
+        {
+            name: 'nails_2',
+            url: 'images/portfolio/unas_2.jpeg',
+            show: true
+        },
+        {
+            name: 'nails_1',
+            url: 'images/portfolio/unas_1.jpeg',
+            show: false
         },
         {
             name: 'nails_6',
@@ -100,6 +100,26 @@ var portfolio_data = {
         {
             name: 'pestañas_2',
             url: 'images/portfolio/pestanas_2.jpeg',
+            show: true
+        },
+        {
+            name: 'pestañas_3',
+            url: 'images/portfolio/pestanas_3.jpeg',
+            show: true
+        },
+        {
+            name: 'pestañas_4',
+            url: 'images/portfolio/pestanas_4.jpeg',
+            show: true
+        },
+        {
+            name: 'pestañas_5',
+            url: 'images/portfolio/pestanas_5.jpeg',
+            show: true
+        },
+        {
+            name: 'pestañas_6',
+            url: 'images/portfolio/pestanas_6.jpeg',
             show: true
         },
     ]
---
+++ js/promos-data.js
@@ -0,0 +1,19 @@
+var promos_data = {
+    promos: [
+        {
+            name: 'promocion_dia_mama',
+            url: 'images/promos/promo_mani_pedi.jpeg',
+            show: true
+        },
+        {
+            name: 'promocion_mani_pedi',
+            url: 'images/promos/promo_dia_mama.jpeg',
+            show: true
+        },
+        {
+            name: 'promo_tarjeta_regalo',
+            url: 'images/promos/promo_tarjeta_regalo.jpeg',
+            show: true
+        }
+    ]
+}
