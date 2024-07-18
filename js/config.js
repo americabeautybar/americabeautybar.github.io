dbea06cdf6c892813b3ed846c06415f96d1e8787
Alejandro <alejhandro.de@gmail.com>
2024-07-17 20:57:41 -0600

    updating fidelity card url

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 var CONFIG = {
     APP: {
-        FIDELITY_API_URL: "https://script.google.com/macros/s/AKfycbyT1Qgy0Y0Dk5z8BzMy78M8idbOKdyIj45ZHBT_GyhVeS-bcY7T8IB2IONxmJQkmPycEw/exec",
+        FIDELITY_API_URL: "https://script.google.com/macros/s/AKfycbxZdONPTtEGD7k8KTNJJlW4adhaZtAC88XhLBaoIIoYyN_0BUZQJPNGVBVI727GqBwtpA/exec",
         GIFTCARD_API_URL: "https://script.google.com/a/~/macros/s/AKfycbx-ZPqMh7C3rPUCxAqBZElyG4dUJ0w4ZSwp9VjSy-nZ1b4oM-Olbcj4R-RpLKC3sMPyug/exec",
         FIDELITY_EDGE_URL: "https://qeggtkpneycwsvszhuyu.supabase.co/functions/v1",
         SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFlZ2d0a3BuZXljd3N2c3podXl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTUyMjE5MjcsImV4cCI6MjAzMDc5NzkyN30.nmw2M42yDOgBaoD9jHhizWs06d27qbu8-aGYcO1b_V8",
